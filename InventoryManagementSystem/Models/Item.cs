4711d99456d107f145a98f946805dda4e1d6761f
dhearizky12 <dherizdhea@gmail.com>
2023-05-23 23:22:02 +0700

    add view model upload image in item

--- InventoryManagementSystem/Models/Item.cs
+++ InventoryManagementSystem/Models/Item.cs
@@ -12,13 +12,22 @@ public class Item
     public string? KodeItem { get; set; }
     [Required]
     public string? Name { get; set; }
+
+    public string? PicturePath { get; set; }
     public string? Description { get; set; }
     public bool Availability { get; set; } = true;
     public int CategoryId { get; set; }
     public int SubCategoryId { get; set; }
-    public virtual Category? Category { get; set; }    
+    public virtual Category? Category { get; set; }
     public virtual SubCategory? SubCategory { get; set; }
     public int SupplierId { get; set; }
     public virtual Supplier? Supplier { get; set; }
 }
+
+public class ItemViewModel : Item
+{
+    public IFormFile? PIcture {get;set;}
+}
+
+
 
