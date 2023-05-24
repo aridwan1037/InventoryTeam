3e470619e248fc001f44f5ac30acb113c5dd2e75
aridwan1037 <128478985+aridwan1037@users.noreply.github.com>
2023-05-24 23:43:14 +0700

    AutoGenerateKodeItem

--- InventoryManagementSystem/Models/Item.cs
+++ InventoryManagementSystem/Models/Item.cs
@@ -8,7 +8,6 @@ public class Item
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int IdItem { get; set; }
-    [Required]
     public string? KodeItem { get; set; }
     [Required]
     public string? Name { get; set; }
