adc538e83a61ac487307ded4593922096f9014f7
dhearizky12 <dherizdhea@gmail.com>
2023-05-24 22:11:55 +0700

    Prepare git ignore

--- InventoryManagementSystem/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace InventoryManagementSystem.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
